053928c56f6754bd332e062a6628d222c399feaf
305ak <jordan@nerd.com>
2019-02-04 18:58:48 -0500

    first draft

---
+++ src/components/JobSearchResults.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class JobSearchResults extends React.Component {
+  render(){
+    const character = this.props.character;
+
+
+    return(
+    <div>
+
+HI
+    </div>
+
+    )
+
+  }
+
+}
+
+export default JobSearchResults;
